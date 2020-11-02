ee9641de118e43f99c88fed6b2c937aa1ccff761
k2 <kevins@kaskada.com>
2020-11-01 16:28:57 -0800

    more test data

--- gcp/google_ocr.py
+++ gcp/google_ocr.py
@@ -1,3 +1,4 @@
+judgements = ['PERFECT', 'GREAT', 'GOOD', 'BAD', 'MISS']
 def detect_text(path):
     """Detects text in the file."""
     from google.cloud import vision
@@ -9,7 +10,8 @@ def detect_text(path):
 
     image = vision.Image(content=content)
 
-    response = client.text_detection(image=image)
+    response = client.document_text_detection(image=image) 
+
     texts = response.text_annotations
     print('Texts:')
 
@@ -42,31 +44,50 @@ def get_text_data(path):
     texts = response.text_annotations
     return texts
 
-def starts_with_alpha(text):
-    return text[0].isalpha
+def is_left(midpoint, vertices):
+    return midpoint[0] >= vertices[1].x
 
-def compare_coordinates(alpha_coordinates, numeric_coordinates):
-    total = 0
-    for alpha_c in alpha_coordinates:
-        for numeric_c in numeric_coordinates:
-            total += abs(alpha_c.y - numeric_c.y)
-    return total
+def same_row(midpoint, vertices):
+    return midpoint[1] >= vertices[0].y and midpoint[1] <= vertices[2].y
+
+def isnumericlike(numeric):
+    for digit in numeric:
+        if not digit.isnumeric() and not digit == 'O':
+            return False
+    return True
+
+
+def find_judgement_count(texts, judgement):
+    current = None
+    midpoint = get_midpoint(judgement.bounding_poly.vertices)
+    for text in texts:
+        text_vertices = text.bounding_poly.vertices
+        if isnumericlike(text.description) and is_left(midpoint, text_vertices) and same_row(midpoint, text_vertices):
+            current = text
+    if current == None:
+        return 'Not found'
+    else:
+        return current.description
+
+def get_midpoint(vertices):
+    mid_x = (vertices[0].x + vertices[1].x) / 2
+    mid_y = (vertices[0].y + vertices[2].y) / 2
+    return (mid_x, mid_y)
+
 
 def process_texts(texts):
     text_pairs = []
-    for alpha_text in texts:
-        closest = (None, 10000000000)
-        if alpha_text.description.isalpha():
-            for numeric_text in texts:
-                if numeric_text.description.isnumeric():
-                    difference = compare_coordinates(alpha_text.bounding_poly.vertices, numeric_text.bounding_poly.vertices)
-                    if difference < closest[1]:
-                        closest = (numeric_text, difference)
-            text_pairs.append((alpha_text.description, closest[0].description))
+    for text in texts:
+        if text.description in judgements:
+            jc_description = find_judgement_count(texts, text)
+            text_pairs.append((text.description, jc_description))
+        # todo: Handle non judgement text
     return text_pairs
 
-img_path = "/home/simo/Development/pumpocr/img/baroque_test.png"
-text_data = get_text_data(img_path)
-pairs = process_texts(text_data)
-for pair in pairs:
-    print("{}\n".format(pair))
+
+img_path = "/home/simo/Development/pumpocr/img/cleaner_rotated_test.jpg"
+detect_text(img_path)
+#text_data = get_text_data(img_path)
+#pairs = process_texts(text_data)
+#for pair in pairs:
+#    print("{}\n".format(pair))
